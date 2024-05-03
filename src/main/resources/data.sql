dd40be4260f317c2d070b7be87be00d0f7da1001
Kyle Grobbelaar <grobbelaarkyle@gmail.com>
2024-05-03 13:25:14 +0200

    added users and products

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -32,16 +32,45 @@ INSERT INTO Persons( NAME, EMAIL, CONTACT, AGE) VALUES ('kyle', 'kyle@mail.com',
 INSERT INTO Persons( NAME, EMAIL, CONTACT, AGE) VALUES ('john', 'john@mail.com', '+27812345678', 32);
 INSERT INTO Persons( NAME, EMAIL, CONTACT, AGE) VALUES ('doe', 'doe@mail.com', '+27616541234', 25);
 INSERT INTO Persons( NAME, EMAIL, CONTACT, AGE) VALUES ('bremmly', 'bremmly@mail.com', '+27747893045', 68);
+INSERT INTO Persons( NAME, EMAIL, CONTACT, AGE) VALUES ('smithy', 'smithy1@mail.com', '+27747893046', 45);
+INSERT INTO Persons( NAME, EMAIL, CONTACT, AGE) VALUES ('johnson', 'johnson2@mail.com', '+27747893047', 32);
+INSERT INTO Persons( NAME, EMAIL, CONTACT, AGE) VALUES ('miller', 'miller3@mail.com', '+27747893048', 55);
+INSERT INTO Persons( NAME, EMAIL, CONTACT, AGE) VALUES ('thompson', 'thompson4@mail.com', '+27747893049', 38);
+INSERT INTO Persons( NAME, EMAIL, CONTACT, AGE) VALUES ('brown', 'brown5@mail.com', '+27747893050', 29);
+INSERT INTO Persons( NAME, EMAIL, CONTACT, AGE) VALUES ('davis', 'davis6@mail.com', '+27747893051', 42);
+INSERT INTO Persons( NAME, EMAIL, CONTACT, AGE) VALUES ('martinez', 'martinez7@mail.com', '+27747893052', 51);
+INSERT INTO Persons( NAME, EMAIL, CONTACT, AGE) VALUES ('anderson', 'anderson8@mail.com', '+27747893053', 97);
+INSERT INTO Persons( NAME, EMAIL, CONTACT, AGE) VALUES ('wilson', 'wilson9@mail.com', '+27747893054', 34);
+
 
 INSERT INTO Products( TYPE, NAME, CURRENT_BALANCE, EMAIL )
-    VALUES ( 'SAVINGS', 'kyle', 5600, 'kyle@mail.com' );
+VALUES ( 'SAVINGS', 'kyle', 5600, 'kyle@mail.com' );
 INSERT INTO Products( TYPE, NAME, CURRENT_BALANCE, EMAIL )
-    VALUES ( 'RETIREMENT', 'john', 21000, 'john@mail.com' );
+VALUES ( 'RETIREMENT', 'john', 21000, 'john@mail.com' );
 INSERT INTO Products( TYPE, NAME, CURRENT_BALANCE, EMAIL )
-    VALUES ( 'SAVINGS', 'john', 1500, 'john@mail.com' );
+VALUES ( 'SAVINGS', 'john', 1500, 'john@mail.com' );
 INSERT INTO Products( TYPE, NAME, CURRENT_BALANCE, EMAIL )
-    VALUES ( 'SAVINGS', 'doe', 32500, 'doe@mail.com' );
+VALUES ( 'SAVINGS', 'doe', 32500, 'doe@mail.com' );
 INSERT INTO Products( TYPE, NAME, CURRENT_BALANCE, EMAIL )
-    VALUES ( 'SAVINGS', 'bremmly', 57800, 'bremmly@mail.com' );
+VALUES ( 'SAVINGS', 'bremmly', 57800, 'bremmly@mail.com' );
 INSERT INTO Products( TYPE, NAME, CURRENT_BALANCE, EMAIL )
-    VALUES ( 'RETIREMENT', 'bremmly', 21000, 'bremmly@mail.com' );
+VALUES ( 'RETIREMENT', 'bremmly', 21000, 'bremmly@mail.com' );
+INSERT INTO Products(TYPE, NAME, CURRENT_BALANCE, EMAIL)
+VALUES ('SAVINGS', 'smithy', 1230, 'smithy1@mail.com');
+INSERT INTO Products(TYPE, NAME, CURRENT_BALANCE, EMAIL)
+VALUES ('RETIREMENT', 'johnson', 5600, 'johnson2@mail.com');
+INSERT INTO Products(TYPE, NAME, CURRENT_BALANCE, EMAIL)
+VALUES ('SAVINGS', 'miller', 970, 'miller3@mail.com');
+INSERT INTO Products(TYPE, NAME, CURRENT_BALANCE, EMAIL)
+VALUES ('RETIREMENT', 'thompson', 0, 'thompson4@mail.com');
+INSERT INTO Products(TYPE, NAME, CURRENT_BALANCE, EMAIL)
+VALUES ('SAVINGS', 'brown', 0, 'brown5@mail.com');
+INSERT INTO Products(TYPE, NAME, CURRENT_BALANCE, EMAIL)
+VALUES ('RETIREMENT', 'davis', 21, 'davis6@mail.com');
+INSERT INTO Products(TYPE, NAME, CURRENT_BALANCE, EMAIL)
+VALUES ('SAVINGS', 'martinez', 0, 'martinez7@mail.com');
+INSERT INTO Products(TYPE, NAME, CURRENT_BALANCE, EMAIL)
+VALUES ('RETIREMENT', 'anderson', 48, 'anderson8@mail.com');
+INSERT INTO Products(TYPE, NAME, CURRENT_BALANCE, EMAIL)
+VALUES ('SAVINGS', 'wilson', 0, 'wilson9@mail.com');
+
